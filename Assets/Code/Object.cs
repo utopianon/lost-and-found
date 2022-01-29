99d8016365768d499aae1188989f7fd1339ef034
sereia belissima <antonio.omt@gmail.com>
2022-01-29 16:25:56 +0200

    lots of things

---
+++ Assets/Code/Object.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Object", menuName = "Object")]
+public class Object : ScriptableObject
+{
+    public string name;
+    public Sprite picture;
+    public string Description;
+}
---
+++ Assets/Code/Profile.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Profile : MonoBehaviour
+{
+    [SerializeField]
+    private Object obj;
+
+    [SerializeField]
+    private GameObject nameField;
+    [SerializeField]
+    private GameObject bioField;
+    [SerializeField]
+    private GameObject picField;
+
+    private void Start()
+    {
+        
+    }
+}
